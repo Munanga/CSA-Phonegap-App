3fb655d7e39b9430a5e766eacc3ed28de1e06ffb
Ethan Rockwood <drockwood94@gmail.com>
2017-04-10 10:17:42 -0500

    Add Firebase example to index.js and include firebase js files in index.html

--- RatingApp/platforms/browser/www/js/index.js
+++ RatingApp/platforms/browser/www/js/index.js
@@ -47,3 +47,33 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBFx36R9V2DPfeaRtdXhZDI7gUAchSfVcw",
+    authDomain: "macombfoodratings.firebaseapp.com",
+    databaseURL: "https://macombfoodratings.firebaseio.com",
+    projectId: "macombfoodratings",
+    storageBucket: "macombfoodratings.appspot.com",
+    messagingSenderId: "950556034085"
+};
+firebase.initializeApp(config);
+
+//Test firebase storage access
+var storage = firebase.storage();
+var storageRef = storage.ref('menus/chicksOnTheSquare/square-menu-appetizers-2017.jpg');
+storageRef.getDownloadURL().then(function(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = function(event) {
+        var blob = xhr.response;
+    };
+    xhr.open('GET', url);
+    xhr.send();
+
+    // Or inserted into an <img> element:
+    var img = document.getElementById('myimg');
+    img.src = url;
+}).catch(function(error) {
+    console.error("Failed to get image");
+});
