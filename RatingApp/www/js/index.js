fd58013a1b45fa0b51eeb284e3bbcdf670238cb1
Ethan Rockwood <drockwood94@gmail.com>
2017-04-11 08:03:26 -0500

    Reenable image demo and keep tag list demo\

--- RatingApp/www/js/index.js
+++ RatingApp/www/js/index.js
@@ -66,28 +66,28 @@ var database = firebase.database();
 // This is because these calls return Promises, which can only be acted on
 // Inside of a .then(function(whatever){}); call, as calling return inside
 // this only returns another promise.
-// database.ref('/restaurants/').once('value').then(function(snapshot) {
-//     imgPath = snapshot.child('chicksOnTheSquare').child('menuURL').val();
+database.ref('/restaurants/').once('value').then(function(snapshot) {
+    imgPath = snapshot.child('chicksOnTheSquare').child('menuURL').val();
 
-//     // Test firebase storage access
-//     var storage = firebase.storage();
-//     var storageRef = storage.ref(imgPath + 'square-menu-appetizers-2017.jpg');
-//     storageRef.getDownloadURL().then(function(url) {
-//         var xhr = new XMLHttpRequest();
-//         xhr.responseType = 'blob';
-//         xhr.onload = function(event) {
-//             var blob = xhr.response;
-//         };
-//         xhr.open('GET', url);
-//         xhr.send();
+    // Test firebase storage access
+    var storage = firebase.storage();
+    var storageRef = storage.ref(imgPath + 'square-menu-appetizers-2017.jpg');
+    storageRef.getDownloadURL().then(function(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = 'blob';
+        xhr.onload = function(event) {
+            var blob = xhr.response;
+        };
+        xhr.open('GET', url);
+        xhr.send();
 
-//         // Or inserted into an <img> element:
-//         var img = document.getElementById('myimg');
-//         img.src = url;
-//     }).catch(function(error) {
-//         console.error("Failed to get image");
-//     });
-// });
+        // Or inserted into an <img> element:
+        var img = document.getElementById('myimg');
+        img.src = url;
+    }).catch(function(error) {
+        console.error("Failed to get image");
+    });
+});
 
 database.ref('/tags/').once('value').then(function(snapshot) {
     updateTagList(snapshot.val());
